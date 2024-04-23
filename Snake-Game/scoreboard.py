bd119aa73fda5a7f67a361910eaefcbd4dfb3504
Nolan Brand <115044761+nolanbrand@users.noreply.github.com>
2024-04-22 20:24:35 -0500

    New and Improved Snake Game
    Added:
    
    -Tracking of user high score for current game session and when game is reopened after closing.
    
    -When player dies, game continually runs on a loop. Game is closed by closing the window.

--- Snake-Game/scoreboard.py
+++ Snake-Game/scoreboard.py
@@ -1,6 +1,10 @@
 from turtle import Turtle
+with open('data.txt') as file:
+    current_highscore = int(file.read())
+
 ALIGNMENT = 'center'
 FONT = ('Arial', 20, 'normal')
+
 
 class Scoreboard(Turtle):
     def __init__(self):
@@ -10,16 +14,20 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.score = 0
+        self.high_score = current_highscore
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(arg=f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(arg=f"Score: {self.score}   High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write('GAME OVER', move=False, align=ALIGNMENT, font=FONT)
-
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open('data.txt', mode='w') as data:
+                data.write(f'{self.high_score}')
+        self.score = 0
+        self.update_scoreboard()
 
     def add_score(self):
         self.clear()
